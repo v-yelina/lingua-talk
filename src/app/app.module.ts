6c59f18773a15a84d4ef6a2046d88d97857d925c
unknown <heytridvaodin@gmail.com>
2023-06-01 13:27:17 +0200

    feat: create games page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
+import { GamesComponent } from './components/games/games.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent
+    MainPageComponent,
+    GamesComponent
   ],
   imports: [
     BrowserModule,
