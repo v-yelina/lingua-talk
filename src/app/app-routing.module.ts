c5d5cde317b55fc6cd5e11f9485077e8aab45e80
unknown <heytridvaodin@gmail.com>
2023-06-01 13:44:00 +0200

    feat: setup routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChooseOneComponent } from './components/choose-one/choose-one.component';
+import { GamesComponent } from './components/games/games.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'main', component: MainPageComponent },
+{
+  path: 'games', component: GamesComponent,
+},
+{
+  path: 'choose-one', component: ChooseOneComponent,
+},
+{ path: '', redirectTo: '/main', pathMatch: 'full' },
+  // TODO { path: '**', component: NotFoundPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
