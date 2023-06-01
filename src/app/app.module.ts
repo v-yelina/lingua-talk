3d5691d581340f7a6c3a803f741bc34f470bd303
unknown <heytridvaodin@gmail.com>
2023-06-01 13:28:08 +0200

    feat: create choose one game component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { GamesComponent } from './components/games/games.component';
+import { ChooseOneComponent } from './components/choose-one/choose-one.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-    GamesComponent
+    GamesComponent,
+    ChooseOneComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/choose-one/choose-one.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-choose-one',
+  templateUrl: './choose-one.component.html',
+  styleUrls: ['./choose-one.component.scss']
+})
+export class ChooseOneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
